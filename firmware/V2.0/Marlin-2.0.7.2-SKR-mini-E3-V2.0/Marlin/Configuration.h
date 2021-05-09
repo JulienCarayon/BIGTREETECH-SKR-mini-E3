b4e3de976f12eaf37684ad91d1a9f6764c51f522
Julien Carayon <78759372+JulienCarayon@users.noreply.github.com>
2021-05-09 19:04:08 +0200

    Changes

--- firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Configuration.h
+++ firmware/V2.0/Marlin-2.0.7.2-SKR-mini-E3-V2.0/Marlin/Configuration.h
@@ -981,7 +981,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -51.112, -8.3, -2 }
+#define NOZZLE_TO_PROBE_OFFSET { -51.112, -8.3, -1.00 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
@@ -1276,7 +1276,7 @@
 #if EITHER(AUTO_BED_LEVELING_LINEAR, AUTO_BED_LEVELING_BILINEAR)
 
   // Set the number of grid points per dimension.
-  #define GRID_MAX_POINTS_X 5
+  #define GRID_MAX_POINTS_X 7
   #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   // Probe along the Y axis, advancing X after each column
